cc278527723ea01486e98eeede5900f9bc85e0e9
Steve Cobb <scobb1@utexas.edu>
2014-10-10 11:08:45 -0500

    Fixing unsubscribe bug

--- Blog/src/blog/dao/SubscriberDAO.java
+++ Blog/src/blog/dao/SubscriberDAO.java
@@ -12,48 +12,59 @@ import blog.services.PMF;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
-public enum SubscriberDAO {
+public enum SubscriberDAO
+{
 	INSTANCE;
-	public List<Subscriber> getSubscribers() {
+	public List<Subscriber> getSubscribers()
+	{
 		/** returns list of subscribers or null on error **/
 		List<Subscriber> Subscribers = null;
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(Subscriber.class);
-		try {
+		try
+		{
 			Subscribers = (List<Subscriber>) query.execute();
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			// handled by null return value
 
-		} finally {
+		} finally
+		{
 			pm.close();
 		}
 		return Subscribers;
 	}
 
-	public Subscriber getSubscriber(String email) {
+	public Subscriber getSubscriber(String email)
+	{
 		/** returns Subscriber or null if subscriber cannot be found **/
 		Key key = KeyFactory.createKey(Subscriber.class.getSimpleName(), email);
 
 		Subscriber found = null;
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 
-		try {
+		try
+		{
 			found = pm.getObjectById(Subscriber.class, key);
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			// nothing to do - return value should take care of it
-		} finally {
+		} finally
+		{
 			pm.close();
 		}
 		return found;
 	}
 
-	public boolean addSubscriber(String name, String email) {
+	public boolean addSubscriber(String name, String email)
+	{
 		/**
-		 * returns true on successful add; false on already exists or
-		 * unsuccessful add
+		 * returns true on successful add; false on already exists or unsuccessful
+		 * add
 		 **/
 		Subscriber found = getSubscriber(email);
-		if (found != null) {
+		if (found != null)
+		{
 			// e-mail already exists for some subscriber
 			return false;
 		}
@@ -61,42 +72,45 @@ public enum SubscriberDAO {
 		Subscriber newSub = new Subscriber(name, key);
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 
-		try {
+		try
+		{
 			pm.makePersistent(newSub);
 			return true;
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			return false;
-		} finally {
+		} finally
+		{
 			pm.close();
 		}
 	}
 
-	public boolean removeSubscriber(String email) {
+	public boolean removeSubscriber(String email)
+	{
 		/**
 		 * returns true on successful removal; false if couldn't be found or
 		 * couldn't be removed
 		 **/
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Key key = KeyFactory.createKey(Subscriber.class.getSimpleName(), email);
+
 		Subscriber found;
-		try {
+		try
+		{
 			found = pm.getObjectById(Subscriber.class, key);
-		}
-		catch (Exception e) { 
-			return false;
-		}
-		finally {
-			pm.close();
-		}
-		if (found == null) {
-			return false;
-		}
-		try {
+
+			if (found == null)
+			{
+				return false;
+			}
 			pm.deletePersistent(found);
 			return true;
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			return false;
-		} finally {
+
+		} finally
+		{
 			pm.close();
 		}
 	}
